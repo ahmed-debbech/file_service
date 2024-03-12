b8726c8f60c3f337f58f4d5d3b3025116cf2899c
ahmed-debbech <debbech.ahmed@gmail.com>
2024-03-12 13:36:24 +0100

    refactor logic implementation in websocket file js

--- websocket.js
+++ websocket.js
@@ -1,16 +1,43 @@
 const ws = require('ws');
 
+let sockets = new Map()
+
+function implementation(key, message){
+    if(!sockets.has(key)) return;
+
+    console.table(message.chunk)
+    if(message.chunk.length == 0) {return;}
+
+    let uin = new Uint8Array(message.chunk)
+
+    fs.appendFileSync("downs/"+message.fileName, uin)
+}
+
+
 
 function mountPoint(request, socket, head){
+    
+    let path = request.url.startsWith('/ws')
+    path = request.url.includes("?id")
 
-    if(request.url.startsWith('/ws')){
-        console.log("mounted")
+    if(path){
+        let key = request.url.split("?")[1].split("=")[1]
+        console.log("initiating socket for id: " + key)
         const wsServer = new ws.Server({ noServer: true });
         wsServer.on('connection', socket => {
             socket.on('message', message => {
-                console.log(message.toString())
+                let msg = message.toString()
+                //implementation(key, msg)
             });
         });
-    } 
+        wsServer.handleUpgrade(request, socket, head, function done(ws) {
+            wsServer.emit('connection', ws, request);
+        });
+        sockets.set(key, wsServer)
+        console.log("socket "+key+" mounted!")
+        console.log(sockets)
+    }else{
+        console.log("unable to initiate socket")
+    }
 }
 module.exports = {mountPoint}
